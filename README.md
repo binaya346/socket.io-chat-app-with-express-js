32946c955c4456f97c3f8976efb2e381e30e32ed
Binaya Rijal <rijalbinaya2@gmail.com>
2020-04-25 01:47:13 +0545

    Adds socket broadcasting

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Socket.IO-chat-app-with-node
+# Socket.io-chat-app-with-express
+
+# This project is made and shared for learning purpose.
+    It includes simple use of express js and socket io and with vanilla javascript in front end. Simple css design. 
+
+# Running app
+    1. npm install
+    2. nodemon | node index
+
+    App will run on port 4000. 
+
+# Thank You...
--- public/js/socket.js
+++ public/js/socket.js
@@ -5,18 +5,27 @@ const message = document.getElementById("message");
 const username = document.getElementById("username");
 const btn = document.getElementById("send");
 const output = document.getElementById("output");
+const broadcast = document.getElementById("broadcast");
 
 //Emit data to socket
 
 btn.addEventListener("click", () => {
   socket.emit("chat", {
     message: message.value,
-    handle: username.value,
+    username: username.value,
   });
+});
+
+message.addEventListener("keypress", () => {
+  socket.emit("typing", username.value);
 });
 
 //Listen for socket data
 socket.on("chat", (data) => {
+  broadcast.innerHTML = "";
   output.innerHTML +=
-    "<p><span>" + data.handle + ":</span>" + data.message + "</p>";
+    "<p><span>" + data.username + ":</span>" + data.message + "</p>";
+});
+socket.on("typing", (data) => {
+  broadcast.innerHTML = "<p><em>" + data + " is typing message" + "</em></P>";
 });
